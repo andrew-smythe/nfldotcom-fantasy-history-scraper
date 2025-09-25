aa815e1f5962420217f5ddf8a931814434068e12
andrew-smythe <andrew.k.smythe@gmail.com>
2025-09-25 12:54:34 -0700

    Version 1.0 complete
    Evaluating pages with puppeteer should work better now (allows for
    retries on page load errors).
    
    Fixed several bugs from earlier iterations

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 # nfldotcom-fantasy-history-scraper
 Long, but fairly self-explanatory title
 
+## Description
+
+This script scrapes NFL.com fantasy football and imports the following into a MySQL database:
+
+* Teams
+* Weekly Player Stats
+* Matchups
+* Trades
+* Draft Picks
+
+It requires an NFL.com fantasy login, and a MySQL database with the correct schema pre-setup (see schema/nerdherd.sql).
+
+## Disclaimer
+
+This is very much a quick and dirty script. It was built specifically for my own personal use, and I cannot guarantee it will work if you try to use it on another league.
+
+If you did want to use it on a different league, this probably would be a good base to build off of.
+
 ## How to Make the Scrape
 
 1. Make sure you have node installed on your PC.
 
 2. Use the SQL schema in /schema/nerdherd.sql to create the structure in your MySQL database of choice.
 
-3. Run the following:
+3. Create a settings.json file. See settings.json.example for an example of which fields are needed. This is required for the script to run correctly.
+
+4. Run the following:
   ```bash
   npm install
   node main.js
--- main.js
+++ main.js
@@ -6,75 +6,92 @@ const matchups = require('./lib/matchups.js');
 const trades = require('./lib/trades.js');
 const { resolve } = require('path');
 const draft = require('./lib/draft.js');
+const fs = require('fs');
 
 const baseUrl = 'https://fantasy.nfl.com/league/33863/history';
 const screenshotPath = 'screenshots/';
 
-const firstYear = 2010;
-const lastYear = 2022;
-
 async function main() {
-    const mysql = db.makeDb();
-    
-    // Test connection
-    mysql.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-        if (error) throw error;
-        console.log('Connected to database');
-    });
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-    });
+    try {
+        let settings = JSON.parse(fs.readFileSync('settings.json'));
+        const mysql = db.makeDb(settings.db.host, settings.db.user, settings.db.password, settings.db.database);
+        
+        // Test connection
+        mysql.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+            if (error) throw error;
+            console.log('Connected to database');
+        });
 
-    // Load Main Page
-    await page.goto(baseUrl);
-    let title = await page.evaluate(() => {
-        return document.title;
-    });
+        const browser = await puppeteer.launch(/*{'headless': false}*/);
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(10000);
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+        });
 
-    // Sign In
-    if (title === 'NFL Account Sign In') {
-        await signin.signin(page);
-        await page.screenshot({ path: screenshotPath + 'login.png', fullPage: true });
-        console.log('Logged into NFL.com');
+        // Load Main Page
+        await page.goto(baseUrl);
+        await page.screenshot({ path: screenshotPath + 'before-login.png', fullPage: true });
+
+        await page.waitForSelector('input[inputmode=email]', { visible: true, timeout: 0 });
+        let title = await page.evaluate(() => {
+            return document.title;
+        });
+
+        await page.screenshot({ path: screenshotPath + 'first-login.png', fullPage: true });
+
+        // Sign In
+        if (title === 'NFL Account Sign In') {
+
+                await signin.signin(page, settings.nflUsername, settings.nflPassword);
+                await page.screenshot({ path: screenshotPath + 'login.png', fullPage: true });
+                await page.waitForSelector('a[href="/myleagues"]', { visible: true, timeout: 0 });        
+                console.log('Logged into NFL.com');
+
+            // Get Teams
+            let managers = [];
+            for (let year = settings.firstYear; year <= settings.lastYear; year++) {
+                await page.goto(baseUrl + '/' + year + '/owners');
+                await managers.push(await teams.get(page, year));
+                await page.screenshot({ path: screenshotPath + 'teams-' + year + '.png', fullPage: true });
+            }
+
+            // Update teams in DB
+            console.log('CREATE AND UPDATE TEAMS');
+            await teams.createAndUpdateTeams(mysql, managers);
+        
+            // Download all weekly player stats into DB
+            console.log('DOWNLOAD ALL WEEKLY PLAYER STATS');
+            await teams.downloadAllWeeklyPlayerStats(mysql, page, baseUrl, managers);
+        
+            // Download all player matchups into DB
+            console.log('DOWNLOAD ALL WEEKLY MATCHUPS');
+            await matchups.downloadAllMatchups(mysql, page, baseUrl, managers);
+        
+            // Download all trades into DB
+            console.log('DOWNLOAD ALL TRADES');
+            await trades.downloadAllTrades(mysql, page, baseUrl, firstYear, lastYear);
+        
+            // Download all draft results into DB
+            console.log('DOWNLOAD ALL DRAFT RESULTS');
+            await draft.downloadAllDraftResults(mysql, page, baseUrl, firstYear, lastYear);
+        }
+        else {
+            console.log('UNABLE TO SIGN INTO NFL.com. Check screenshot to see if login page has changed.');
+            console.log('Page title was ' + title);
+        }
+
+        await browser.close();
+        console.log('Browser instance closed.');
+
+        await mysql.close();
+        console.log('Connection to DB closed.');
     }
-
-    // Get Teams
-    let managers = [];
-    for (let year = firstYear; year <= lastYear; year++) {
-        await page.goto(baseUrl + '/' + year + '/owners');
-        await managers.push(await teams.get(page, year));
-        await page.waitForTimeout(2000);
+    catch (e) {
+        console.error('ERROR - Could not read settings from settings.json');
+        console.error(e);
     }
-    
-    // Update teams in DB
-    console.log('CREATE AND UPDATE TEAMS');
-    await teams.createAndUpdateTeams(mysql, managers);
-
-    // Download all weekly player stats into DB
-    console.log('DOWNLOAD ALL WEEKLY PLAYER STATS');
-    await teams.downloadAllWeeklyPlayerStats(mysql, page, baseUrl, managers);
-
-    // Download all player matchups into DB
-    console.log('DOWNLOAD ALL WEEKLY MATCHUPS');
-    await matchups.downloadAllMatchups(mysql, page, baseUrl, managers);
-
-    // Download all trades into DB
-    console.log('DOWNLOAD ALL TRADES');
-    await trades.downloadAllTrades(mysql, page, baseUrl, firstYear, lastYear);
-
-    // Download all draft results into DB
-    console.log('DOWNLOAD ALL DRAFT RESULTS');
-    await draft.downloadAllDraftResults(mysql, page, baseUrl, firstYear, lastYear);
-
-    await browser.close();
-    console.log('Browser instance closed.');
-
-    await mysql.close();
-    console.log('Connection to DB closed.');
 }
 main();
